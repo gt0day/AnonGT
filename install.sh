1644a9df69d64668e1f4dfc56f5ad994ec153b5e
Ghost 0day <83251831+gt0day@users.noreply.github.com>
2023-09-03 01:41:17 +0300

    v0.1.0

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ fi
 
 echo "AnonGT Installer"
 
-rm -r "/usr/share/anongt" > /dev/null 2>&1
-rm -r "/usr/bin/anongt" > /dev/null 2>&1
-rm -r "/var/lib/anongt" > /dev/null 2>&1
+rm -r "/usr/share/anongt" 
+rm -r "/usr/bin/anongt" 
+rm -r "/var/lib/anongt" 
 
 sudo apt update
 sudo apt install -y apt-transport-https lsb-release curl
