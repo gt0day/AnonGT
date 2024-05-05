635c52e1fa96ea489a901630dd8267bf41866795
Ghost 0day <83251831+gt0day@users.noreply.github.com>
2024-05-05 15:02:33 +0000

    Update banner.py

--- core/assets/banner.py
+++ core/assets/banner.py
@@ -41,32 +41,32 @@ logo = f"""
 # banner with version
 def banner():
     banner = f"""
-    {red(logo)}     
+    {red(logo)}
                 {anongt_isactive()}   {tor_isacttive()}
-    {green('''         
-    Let's To Be Anonymous Ghost! 
+    {green('''
+    Let's To Be Anonymous Ghost!
     Usage: anongt <options>
     ---------------------------------------------------------
-         Options  | Descriptions                             
+         Options  | Descriptions
     ---------------------------------------------------------
          start    | Anonymous Mode Start
-         start+   | Start With Secure Tor Bridges                     
+         start+   | Start With Secure Tor Bridges
          stop     | Anonymous Mode Stop
-                -----                      
-         status   | Watch Tor Traffic                        
-         myinfo   | Get Your Information                     
-         chngid   | Change Tor Identity  
+                -----
+         status   | Watch Tor Traffic
+         myinfo   | Get Your Information
+         chngid   | Change Tor Identity
          amitm    | Anti MITM
-                ----- 
-         chngmac  | Changing Mac Address Of All Interfaces
-         rvmac    | Reverting Mac Address Of All Interfaces
+                -----
+         chngmac  | Change Mac Addresses Of All Interfaces
+         rvmac    | Revert Mac Addresses Of All Interfaces
                 -----
          oniongen | Onion Links Generator
-         checko   | Onion Links Checker 
+         checko   | Onion Links Checker
                 -----
-         wipe     | Memory Wipe & Clear Logs                
-         checku   | Check Update                             
-         about    | About US                                 
+         wipe     | Memory Wipe & Clear Logs
+         checku   | Check Update
+         about    | About US
     ---------------------------------------------------------
     ''')}
     """
